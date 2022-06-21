6326896f006b9b5c7033e594cbd9dbf353f22665
IgorDikal <igrdkl@icloud.com>
2022-06-21 16:39:17 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::middleware(['auth'])->group(function (){
+    Route::prefix('admin')->group(function(){
+        Route::resource('/', App\Http\Controllers\Admin\AdminContrller::class);
+    });
+});
+
+require __DIR__.'/auth.php';
